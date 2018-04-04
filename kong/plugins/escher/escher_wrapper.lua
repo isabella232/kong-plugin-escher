8f6af38cc1913bc054c0c3944e92e1b351353f68
bencso <gabor.bencsik13@gmail.com>
2018-04-04 14:19:12 +0200

    create escher wrapper

---
+++ kong/plugins/escher/escher_wrapper.lua
@@ -0,0 +1,58 @@
+local Object = require "classic"
+local Escher = require "escher"
+
+local EscherWrapper = Object:extend()
+
+local function parse_headers(ngx_headers)
+    local headers = {}
+    local mandatoryHeadersToSign = {}
+    for key, value in pairs(ngx_headers) do
+        table.insert(headers, {key, value})
+        if key == 'x-suite-customerid' then
+            table.insert(mandatoryHeadersToSign, key)
+        end
+    end
+
+    return headers, mandatoryHeadersToSign
+end
+
+function EscherWrapper:new(ngx)
+    self.ngx = ngx
+end
+
+function EscherWrapper:authenticate()
+    local escher = Escher:new({
+        ["vendorKey"] = "EMS",
+        ["algoPrefix"] = "EMS",
+        ["hashAlgo"] = "SHA256",
+        ["credentialScope"] = "eu/suite/ems_request",
+        ["authHeaderName"] = "X-Ems-Auth",
+        ["dateHeaderName"] = "X-Ems-Date",
+    })
+
+    local key_db = function(key_name)
+        return "test_secret"
+    end
+
+    local headers, mandatory_headers_to_sign = parse_headers(self.ngx.req.get_headers())
+
+    self.ngx.req.read_body()
+
+    local request = {
+        ["method"] = self.ngx.req.get_method(),
+        ["url"] = self.ngx.var.request_uri,
+        ["headers"] = headers,
+        ["body"] = self.ngx.req.get_body_data()
+    }
+
+    local api_key, err = escher:authenticate(request, key_db, mandatory_headers_to_sign)
+
+    if not api_key then
+        return nil
+    else
+        return api_key, err
+    end
+
+end
+
+return EscherWrapper
---
+++ spec/escher/escher_wrapper_spec.lua
@@ -0,0 +1,130 @@
+local EscherWrapper =  require "kong.plugins.escher.escher_wrapper"
+local Escher = require "escher"
+
+describe("escher wrapper", function()
+
+    local ngx_req_headers = {}
+    local set_headers = function(headers)
+        ngx_req_headers = headers
+    end
+    local ngx_mock = {
+        req = {
+            get_method = function()
+               return "GET"
+            end,
+            get_body_data = function()
+                return ''
+            end,
+            get_headers = function()
+                return ngx_req_headers
+            end,
+            read_body = function() end
+        },
+        var = {
+            request_uri = "request_uri"
+        },
+
+    }
+    local escher_wrapper = EscherWrapper(ngx_mock)
+
+    local current_date = os.date("!%Y%m%dT%H%M%SZ")
+    local config = {
+        algoPrefix      = 'EMS',
+        vendorKey       = 'EMS',
+        credentialScope = 'eu/suite/ems_request',
+        authHeaderName  = 'X-Ems-Auth',
+        dateHeaderName  = 'X-Ems-Date',
+        accessKeyId     = 'test_key',
+        apiSecret       = 'test_secret',
+        date            = current_date,
+    }
+
+    local request_headers = {
+        { "X-Ems-Date", current_date },
+        { "Host", "" }
+    }
+
+    local request = {
+        ["method"] = "GET",
+        ["headers"] = request_headers,
+        ["body"] = '',
+        ["url"] = "request_uri"
+    }
+
+    local escher = Escher:new(config)
+
+    local ems_auth_header = escher:generateHeader(request, {})
+
+    describe("#authenticate", function()
+
+        it("returns nil when escher authentication was not successful", function()
+            set_headers({})
+            assert.are.equal(nil, escher_wrapper:authenticate())
+        end)
+
+        it("should call escher lib authenticate method", function()
+            set_headers({})
+            spy.on(Escher, "authenticate")
+            escher_wrapper:authenticate()
+
+            assert.spy(Escher.authenticate).was.called()
+        end)
+
+        it("should call escher lib authenticate method with proper headers", function()
+
+            local headers = {
+                ["X-Ems-Date"] = current_date,
+                ["X-Ems-Auth"] = ems_auth_header,
+                ["Host"] = '',
+            }
+
+            local expected_request_headers = {
+                { "X-Ems-Date", current_date },
+                { "Host", "" },
+                { "X-Ems-Auth", ems_auth_header },
+            }
+
+            local expected_request = {
+                ["method"] = "GET",
+                ["headers"] = expected_request_headers ,
+                ["body"] = '',
+                ["url"] = "request_uri"
+            }
+
+            local mandatory_headers_to_sign = {}
+
+            set_headers(headers)
+            spy.on(Escher, "authenticate")
+            escher_wrapper:authenticate()
+
+            assert.spy(Escher.authenticate).was.called_with(match._, match.is_same(expected_request), match.is_function(), mandatory_headers_to_sign)
+        end)
+
+        it("should call ngx.req.read_body() when authenticate", function()
+            local escher_auth_func = Escher.authenticate
+            Escher.authenticate = function() end
+
+            spy.on(ngx_mock.req, "read_body")
+            escher_wrapper:authenticate()
+
+            assert.spy(ngx_mock.req.read_body).was.called()
+
+            Escher.authenticate = escher_auth_func
+        end)
+
+        it("should return with api_key when escher authentication was successful", function()
+            local expected_api_key = 'test_key'
+            local headers = {
+                ["X-Ems-Date"] = current_date,
+                ["X-Ems-Auth"] = ems_auth_header,
+                ["Host"] = '',
+            }
+
+            set_headers(headers)
+            local api_key, err = escher_wrapper:authenticate()
+            assert.are.equal(expected_api_key, api_key)
+        end)
+
+    end)
+
+end)
