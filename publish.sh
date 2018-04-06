90e169a85bd23f64815ebb6c6220af13073078c6
bencso <gabor.bencsik13@gmail.com>
2018-04-06 11:04:39 +0200

    Update files to publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 luarocks make
-luarocks pack kong-plugin-wsse
+luarocks pack kong-plugin-escher
 find . -name '*.rockspec' | xargs luarocks upload --api-key=$LUAROCKS_API_KEY
 find . -name '*.all.rock' -delete
 find . -name '*.src.rock' -delete
