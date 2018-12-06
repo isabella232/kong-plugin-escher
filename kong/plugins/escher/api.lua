211f88ec0667dead88f7470c44dc61a7c46b5632
Kovács Ariel <arielkovacs@gmail.com>
2018-12-06 15:12:49 +0100

    INT-0080: Admin api refactor

--- kong/plugins/escher/api.lua
+++ kong/plugins/escher/api.lua
@@ -2,7 +2,7 @@ local crud = require "kong.api.crud_helpers"
 local Crypt = require "kong.plugins.escher.crypt"
 
 local function retrieve_an_escher_plugin_config(plugins_dao)
-    local escher_plugins = plugins_dao:find_page({name = "escher"}, 0, 1)
+    local escher_plugins = plugins_dao:find_page({ name = "escher" }, 0, 1)
 
     return escher_plugins[1].config
 end
@@ -34,32 +34,29 @@ return {
     ["/consumers/:username_or_id/escher_key/:escher_key_name_or_id"] = {
         before = function(self, dao_factory, helpers)
             crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
-            self.params.consumer_id = self.consumer.id
 
             local credentials, err = crud.find_by_id_or_field(
                 dao_factory.escher_keys,
-                { consumer_id = self.params.consumer_id },
+                { consumer_id = self.consumer.id },
                 ngx.unescape_uri(self.params.escher_key_name_or_id),
                 "key"
             )
 
             if err then
                 return helpers.yield_error(err)
-            elseif next(credentials) == nil then
+            elseif #credentials == 0 then
                 return helpers.responses.send_HTTP_NOT_FOUND()
             end
-            self.params.escher_key_name_or_id = nil
 
             self.escher_key = credentials[1]
         end,
 
         GET = function(self, dao_factory, helpers)
-            local escher_key = {}
-            escher_key.id = self.escher_key.id
-            escher_key.consumer_id = self.escher_key.consumer_id
-            escher_key.key = self.escher_key.key
-
-            return helpers.responses.send_HTTP_OK(escher_key)
+            return helpers.responses.send_HTTP_OK({
+                id = self.escher_key.id,
+                consumer_id = self.escher_key.consumer_id,
+                key = self.escher_key.key
+            })
         end,
 
         DELETE = function(self, dao_factory, helpers)
